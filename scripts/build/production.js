4183c393150a3f3047bc70486f4cf4bc26b33c68
Victor Lourng <hello@victorlourng.com>
2014-12-29 01:38:46 -0500

    Let it snow.js!

--- scripts/build/production.js
+++ scripts/build/production.js
@@ -241,7 +241,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/about", {templateUrl: "pages/about.html", controller: "pageController"})
     .when("/contact", {templateUrl: "pages/contact.html", controller: "pageController"})
     // Event Pages
-    .when("/holiday", {templateUrl: "pages/holiday.html", controller: "pageController"})
+    .when("/holiday", {templateUrl: "pages/holiday.html", controller: "holidayController"})
     // Blog
     .when("/projects", {templateUrl: "pages/projects.html", controller: "projectController"})
     .when("/project/item", {templateUrl: "pages/project.html", controller: "projectController"})
@@ -261,6 +261,98 @@ app.controller('pageController', function () {
   // Page-specific scripts
 });
 
+// Angular Holiday Controller
+app.controller('holidayController', function () {
+  console.log("Ho, ho, ho!");
+  // Page-specific scripts
+  var snow = function () {
+  // SNOW!
+  // -> Winter wrap-up planned for never!
+  //    Minification breaks it!
+  // -@ http://thecodeplayer.com/walkthrough/html5-canvas-snow-effect
+      //canvas init
+      var canvas = document.getElementById("snow");
+      var ctx = canvas.getContext("2d");
+      //canvas dimensions
+      var W = window.innerWidth;
+      var H = window.innerHeight;
+      canvas.width = W;
+      canvas.height = H;
+      //snowflake particles
+      var mp = 45; //max particles
+      var particles = [];
+      for(var i = 0; i < mp; i++)
+      {
+          particles.push({
+              x: Math.random()*W, //x-coordinate
+              y: Math.random()*H, //y-coordinate
+              r: Math.random()*4+1, //radius
+              d: Math.random()*mp //density
+          })
+      }
+      //Lets draw the flakes
+      function draw()
+      {
+          ctx.clearRect(0, 0, W, H);
+
+          ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+          ctx.beginPath();
+          for(var i = 0; i < mp; i++)
+          {
+              var p = particles[i];
+              ctx.moveTo(p.x, p.y);
+              ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+          }
+          ctx.fill();
+          update();
+      }
+      //Function to move the snowflakes
+      //angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+      var angle = 0;
+      function update()
+      {
+          angle += 0.01;
+          for(var i = 0; i < mp; i++)
+          {
+              var p = particles[i];
+              //Updating X and Y coordinates
+              //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+              //Every particle has its own density which can be used to make the downward movement different for each flake
+              //Lets make it more random by adding in the radius
+              p.y += Math.cos(angle+p.d) + 1 + p.r/2;
+              p.x += Math.sin(angle) * 2;
+
+              //Sending flakes back from the top when it exits
+              //Lets make it a bit more organic and let flakes enter from the left and right also.
+              if(p.x > W || p.x < 0 || p.y > H)
+              {
+                  if(i%3 > 0) //66.67% of the flakes
+                  {
+                      particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
+                  }
+                  else
+                  {
+                      //If the flake is exitting from the right
+                      if(Math.sin(angle) > 0)
+                      {
+                          //Enter fromth
+                          particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
+                      }
+                      else
+                      {
+                          //Enter from the right
+                          particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
+                      }
+                  }
+              }
+          }
+      }
+      //animation loop
+      setInterval(draw, 100);
+  };
+  // And fire it after definition
+  snow();
+});
 
 // = require bower
 // = require jquery
